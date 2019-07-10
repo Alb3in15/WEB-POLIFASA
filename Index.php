14671162b096ee17daf440e0926689f787ea2296
Alb3in15 <Alb3in15@users.noreply.github.com>
2019-07-10 14:28:43 -0500

    Prueba

--- Index.php
+++ Index.php
@@ -36,7 +36,7 @@
                         <a href="Paginas/Contactanos.html" class="nav-link">Contáctanos</a>
                     </li>
                     <li class="nav-item" >
-                        <a class="nav-link" href="Paginas/Blog.php">Blog Poli</a>
+                        <a class="nav-link" href="Paginas/Blog.php">Blog</a>
                     </li>
                 </ul>
             </div>
